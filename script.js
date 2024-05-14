01897173bf7a76499d9b109ac69f7b37f84c2f83
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-05-14 16:32:20 -0300

    Implementação das funções isPalindrome e arrayMaxMin

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+
+    let palavraSemEspacos = "";
+    let palavraAoContrario = "";
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] != " ") {
+            palavraSemEspacos = palavraSemEspacos + str[i];
+        }
+    }
+
+    for (let i = palavraSemEspacos.length - 1; i >= 0; i--) {
+
+        palavraAoContrario = palavraAoContrario + palavraSemEspacos[i];
+    }
+
+    palavraSemEspacos = palavraSemEspacos.toLowerCase();
+    palavraAoContrario = palavraAoContrario.toLowerCase();
+
+    if (palavraSemEspacos === palavraAoContrario) {
+        return true;
+    }
+    return false;
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+function arrayMaxMin(arr) {
+
+    let valorMax = 0;
+    let valorMin = 0;
+    let arrayRetorno = [];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] > valorMax) {
+
+            valorMax = arr[i];
+        }
+    }
+
+    valorMin = valorMax;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] < valorMin) {
+
+            valorMin = arr[i];
+        }
+    }
+
+    arrayRetorno = [valorMin, valorMax];
+
+    return arrayRetorno;
 }
